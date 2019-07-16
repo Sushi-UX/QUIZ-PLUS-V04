01a5aea88b5f06b013e8378340242286f541a8fb
Sushi-UX <thechoygroup@gmail.com>
2019-07-16 03:40:24 -0400

    Finished inputting questions and answers
    Finished inputting questions and answers

--- Quiz Plus v4/Quiz Plus v4/Systems/QuizManager.swift
+++ Quiz Plus v4/Quiz Plus v4/Systems/QuizManager.swift
@@ -19,5 +19,54 @@ class QuizManager {
     
     
     
-    var questions: [[String:String]] = []
+    var questions: [[String:String]] = [["Question":"This was the only US President to serve more than two consecutive terms.","Answer":"Franklin D. Roosevelt"],// Roosevelt
+    ["Question":"Which of the following countries has the most residents?", "Answer":"Nigeria"],//Nigeria
+    ["Question":"In what year was the United Nations founded?","Answer":"1945"],//1945
+    ["Question":"The Titanic departed from the United Kingdom, where was it supposed to arrive?","Answer":"New York City"],//New York City
+    ["Question":"Which nation produces the most oil?","Answer":"Canada"],//Canada
+    ["Question":"Which country has most recently won consecutive World Cups in Soccer?","Answer":"Brazil"],//Brazil
+    ["Question":"Which of the following rivers is longest?","Answer":"Mississippi"],//Mississippi
+    ["Question":"Which city is the oldest?","Answer":"Mexico City"],//Mexico City
+    ["Question":"Which country was the first to allow women to vote in national elections?","Answer":"Poland"],//Poland
+    ["Question":"Which of these countries won the most medals in the 2012 Summer Games?","Answer":"Great Britian"]//Great Britain
+    ]
+    
+    var answers: [[String]] = [
+    ["George Washington","Franklin D. Roosevelt","Woodrow Wilson","Andrew Jackson"],//2
+        ["Nigeria","Russia","Iran"],//1
+        ["1918","1919","1945","1954"],//3
+        ["Paris","Washington D.C.","New York City"],//3
+        ["Iran","Iraq","Brazil","Canada"],//4
+        ["Italy","Brazil","Argetina"],//2
+        ["Yangtze","Mississippi","Congo","Mekong"],//2
+        ["Mexico City","Cape Town","San Juan"],//1
+        ["Poland","United States","Sweden","Senegal"],//1
+        ["France","Germany","Japan","Great Britian"]//4
+    ]
+    
+    var allQuestions: [QuizObject] = []
+    
+    init() {
+        for a in 0..<questions.count {
+            let trivia = QuizObject(question: questions[a],answer : answers[a])
+            allQuestions.append(trivia)
+        }
+    }
+    
+    func generateUnquieNumber()-> Int {
+        
+        var shuffleQuestions = GKRandomSource.sharedRandom().nextInt(upperBound: self.allQuestions.count)
+        while randomNumberGenerated.contains(shuffleQuestions) {
+            shuffleQuestions = GKRandomSource.sharedRandom().nextInt(upperBound: self.allQuestions.count)
+        }
+        
+        randomNumberGenerated.append(shuffleQuestions)
+        return shuffleQuestions
+    }
+    
+    // Selects random questions
+    func randomQuestion() -> QuizObject {
+        let genneratedQuestion = allQuestions[generateUnquieNumber()]
+        return genneratedQuestion
+    }
 }
